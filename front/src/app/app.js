32883065cbd311278c85bf800a849005f79279c0
Aiton Goldman <agoldma@us.ibm.com>
2016-10-10 15:28:41 -0400

    IndexController and SidebarController now fully functional.
    IndexController now uses the controller_bootstrap() and has a logged in aware home page.  SidebarController can now handle login and logot properly.

--- front/src/app/app.js
+++ front/src/app/app.js
@@ -12,12 +12,16 @@ require('angular-aria');
 require('angular-material');
 //require('angular-ui-bootstrap');
 require('angular-resource');
+require('angular-sanitize');
+require('angular-cookies');
 app = angular.module(
 	'TDApp',
 	[
  	    'ui.router',
             'ngMaterial',
+            'ngCookies',
             'TD_services',
+            'ngSanitize',
             'app.login',/*REPLACEMECHILD*/
 	]
 );
@@ -26,10 +30,9 @@ app = angular.module(
 app.controller(
     'IndexController',    
     function($scope, $location, $http, 
-             $state,$mdSidenav,User) {
-        if(User.logged_in() == false){            
-            $state.go(".login");
-        }
+             $state,$mdSidenav,User, Modals, Utils) {
+        Utils.controller_bootstrap($scope,$state);
+        $scope.User = User;
     }
 );
 
@@ -38,7 +41,8 @@ app.controller(
 app.controller(
     'TitleBarController',    
     function($scope, $location, $http, 
-             $state,$mdSidenav) {        
+             $state,$mdSidenav) {
+        
         $scope.toggle_sidenav=function(){            
             $mdSidenav('left_sidenav').toggle();
         };
@@ -58,6 +62,31 @@ app.controller(
 );
 
 app.controller(
+    'SideBarController',    
+    function($scope, $location, $http, 
+             $state,User,$mdSidenav,Modals,TimeoutResources,$cookies) {
+        $scope.User=User;
+        //$scope.state=$state;
+        $scope.close_sidebar = function(){
+            $mdSidenav('left_sidenav').close();
+        };
+        $scope.logout = function(){            
+            Modals.loading();
+            $cookies.remove('session');
+            User.log_out();                
+            $scope.close_sidebar();
+            
+            $logout_promise = TimeoutResources.Logout(undefined,{site:$state.params.site});
+            $logout_promise.then(function(data){
+                Modals.loaded();
+            }, function(data){                                
+                Modals.loaded();                
+            });
+        };
+    }
+);
+
+app.controller(
     'TestController',    
     function($scope, $location, $http, 
              $state,$mdSidenav) {        
@@ -72,22 +101,23 @@ app.config(function($httpProvider) {
     //$httpProvider.interceptors.push('myHttpInterceptor');
 });
 
-app.factory('myHttpInterceptor', function($q,$injector,$rootScope) {
-    return {
-	'responseError': function(rejection) {
-	    //var StatusModal = $injector.get('StatusModal');
-	    if(rejection.status == -1){
-		rejection.data={};
-		rejection.data.message="HTTP Timeout while getting "+rejection.config.url;
-	    }
-	    //$rootScope.loading = false;
-            console.log('HTTP problems encountered - stand by for more details');
-	    console.log(rejection);
-	    //StatusModal.http_error(rejection.data.message, rejection.data.state_go);	    
-	    return $q.reject(rejection);
-	}
-    };
-});
+
+// app.factory('myHttpInterceptor', function($q,$injector,$rootScope) {
+//     return {
+// 	'responseError': function(rejection) {
+// 	    var Modal = $injector.get('Modal');
+// 	    if(rejection.status == -1){
+// 		rejection.data={};
+// 		rejection.data.message="HTTP Timeout while getting "+rejection.config.url;
+// 	    }
+// 	    //$rootScope.loading = false;
+//             console.log('HTTP problems encountered - stand by for more details');
+// 	    console.log(rejection);
+// 	    //StatusModal.http_error(rejection.data.message, rejection.data.state_go);	    
+// 	    return $q.reject(rejection);
+// 	}
+//     };
+// });
 
 
 app.filter('range', function() {

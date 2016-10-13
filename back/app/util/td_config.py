a2c16e251f9eaa5c5a5ba9f9ed7f1d9c11a24e77
Aiton Goldman <agoldma@us.ibm.com>
2016-10-13 18:54:17 -0400

    simplified by removing get_config()

--- back/app/util/td_config.py
+++ back/app/util/td_config.py
@@ -1,11 +1,8 @@
 from configobj import ConfigObj
 
-def get_config(filename):
-    return ConfigObj(filename)
-
 def assign_loaded_config(app,public_config_filename,secret_config_filename):    
-    app.td_secret_config = get_config(secret_config_filename)
-    app.td_config = get_config(public_config_filename)    
+    app.td_secret_config = ConfigObj(secret_config_filename)
+    app.td_config = ConfigObj(public_config_filename)    
     if 'flask_secret_key' not in app.td_secret_config or app.td_secret_config['flask_secret_key'] == "":
         raise Exception("You didn't configure your flask secret key!")
     app.secret_key = app.td_secret_config['flask_secret_key']

b79d8d1da6ddc54532adeb66b63a4a68e9098947
Aiton Goldman <agoldma@us.ibm.com>
2016-10-13 18:53:30 -0400

    refactored flask app generation

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,56 +1,5 @@
-from flask import Flask
-from sqlalchemy_utils import create_database, database_exists
-from app import td_public, td_secret
-from app.util import db_util,td_config, auth
+from app.util.app_build import get_meta_admin_app, get_admin_app
 from app.util.dispatch import PathDispatcher
-import os
-from app.types import ImportedTables
-from werkzeug.exceptions import BadRequest
-from flask_principal import Principal
-from flask_cors import CORS
-from flask_login import LoginManager
-
-from app import routes
-
-from blueprints import admin_login_blueprint,meta_admin_blueprint,admin_manage_blueprint
-
-def get_generic_app(name):
-    db_url = db_util.generate_db_url(name)
-    if not database_exists(db_url):        
-        return None        
-    app = Flask(name)
-    principals = Principal(app)    
-    app.my_principals = principals
-    app.register_error_handler(BadRequest, lambda e: 'bad request!')        
-    app.config.from_pyfile('app/td_flask.py')    
-    td_config.assign_loaded_config(app,'app/td_public.py','app/td_secret.py')
-    CORS(
-        app,
-        headers=['Content-Type', 'Accept'],
-        send_wildcard=False,
-        supports_credentials=True,
-    )
-    return app
-
-def get_admin_app(name):
-    if name == 'meta_admin':
-        return None
-    app = get_generic_app(name)            
-    if app:
-        db_url = db_util.generate_db_url(name)
-        db_handle = db_util.create_db_handle(db_url, app)
-        app.tables = ImportedTables(db_handle)
-        app.register_blueprint(admin_login_blueprint)
-        app.register_blueprint(admin_manage_blueprint)                        
-        LoginManager().init_app(app)
-        auth.generate_user_loader(app)
-        auth.generate_identity_loaded(app)
-    return app
-
-def get_meta_admin_app():    
-    app = get_generic_app('meta_admin')
-    app.register_blueprint(meta_admin_blueprint)
-    return app
 
 App = PathDispatcher(get_meta_admin_app(), get_admin_app)
 

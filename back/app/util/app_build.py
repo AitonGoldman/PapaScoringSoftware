e2e81342cc36c7f8356da2bfd5c4a934083273a6
Aiton Goldman <agoldma@us.ibm.com>
2016-10-14 12:31:20 -0400

    changed db_util() funcs to use PgInfo

--- back/app/util/app_build.py
+++ back/app/util/app_build.py
@@ -2,6 +2,7 @@ from flask import Flask
 from sqlalchemy_utils import create_database, database_exists
 from app import td_public, td_secret
 from app.util import db_util,td_config, auth
+from app.util.pg_info import build_PgInfo
 from app.util.dispatch import PathDispatcher
 import os
 from app.types import ImportedTables
@@ -15,15 +16,17 @@ from app import routes
 from app.blueprints import admin_login_blueprint,meta_admin_blueprint,admin_manage_blueprint
 
 def get_generic_app(name):
-    db_url = db_util.generate_db_url(name)
+    app = Flask(name)    
+    app.config.from_pyfile('app/td_flask.py')    
+    td_config.assign_loaded_config(app,'app/td_public.py','app/td_secret.py')
+    pg_info = build_PgInfo(app)
+    use_sqlite = app.td_config['sqlite']
+    db_url = db_util.generate_db_url(name,pg_info=pg_info,use_sqlite=use_sqlite)            
     if not database_exists(db_url):        
         return None        
-    app = Flask(name)
     principals = Principal(app)    
     app.my_principals = principals
     app.register_error_handler(BadRequest, lambda e: 'bad request!')        
-    app.config.from_pyfile('app/td_flask.py')    
-    td_config.assign_loaded_config(app,'app/td_public.py','app/td_secret.py')
     CORS(
         app,
         headers=['Content-Type', 'Accept'],
@@ -37,7 +40,9 @@ def get_admin_app(name):
         return None
     app = get_generic_app(name)            
     if app:
-        db_url = db_util.generate_db_url(name)
+        db_url = db_util.generate_db_url(name,
+                                     pg_username=app.td_secret_config['pg_username'],
+                                     pg_password=app.td_secret_config['pg_password'])
         db_handle = db_util.create_db_handle(db_url, app)
         app.tables = ImportedTables(db_handle)
         app.register_blueprint(admin_login_blueprint)
--- back/tests/unit/test_util_auth.py
+++ back/tests/unit/test_util_auth.py
@@ -7,22 +7,7 @@ from flask_principal import Identity
 from flask_login import login_user,LoginManager
 from mock import MagicMock
 
-class UtilAuthTD(unittest.TestCase):
-    def build_mock_tables_query(self):
-        tables_to_make=['User','Role']
-        self.mock_tables = MagicMock()        
-        for table in tables_to_make:
-            setattr(self.mock_tables, table, MagicMock())
-
-            mock_table = getattr(self.mock_tables,table)
-            setattr(table_mock,'query',MagicMock())
-
-            mock_table_query = getattr(mock_table,'query')
-            setattr(table_mock_query,'get',MagicMock())
-
-            mock_table_query_get = getattr(mock_table_query,'get')
-            mock_table_query_get.return_value = self.user_with_roles                    
-    
+class UtilAuthTD(unittest.TestCase):    
     def setUp(self):
         self.app = Flask("dummy")
         self.app.secret_key='poop'
@@ -34,8 +19,7 @@ class UtilAuthTD(unittest.TestCase):
         self.role = self.tables.Role()
         self.role.name = "new_role"
         self.user_with_roles.roles=[self.role]        
-        self.id = Identity('dummy')
-        self.build_mock_tables_query()
+        self.id = Identity('dummy')        
         
     def test_on_identity_loaded_callback(self):         
         with self.app.test_request_context():                                        
@@ -47,8 +31,9 @@ class UtilAuthTD(unittest.TestCase):
             self.assertEqual(len(self.id.provides),2)
             self.assertIsNotNone(self.id.user)
 
-    def test_generate_user_loader(self):         
-        self.app.tables=self.mock_tables
+    def test_generate_user_loader(self):                 
+        self.app.tables=MagicMock()
+        self.app.tables.User.query.get.return_value=self.user_with_roles
         with self.app.test_client() as c:            
             user_loader = auth.generate_user_loader(self.app)
             returned_user = user_loader(1)

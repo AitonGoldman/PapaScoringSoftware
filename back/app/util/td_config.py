39f54473b519bf70e03c8b6cef902606a3e13d36
Aiton Goldman <agoldma@us.ibm.com>
2016-10-14 12:32:08 -0400

    set td_config['sqlite'] to false if not found

--- back/app/util/td_config.py
+++ back/app/util/td_config.py
@@ -6,5 +6,6 @@ def assign_loaded_config(app,public_config_filename,secret_config_filename):
     if 'flask_secret_key' not in app.td_secret_config or app.td_secret_config['flask_secret_key'] == "":
         raise Exception("You didn't configure your flask secret key!")
     app.secret_key = app.td_secret_config['flask_secret_key']
-
+    if 'sqlite' not in app.td_config:
+        app.td_config['sqlite']=False
  

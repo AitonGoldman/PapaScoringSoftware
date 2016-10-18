07d26637d5fb783bfbddbdb59cbfb62f2269485a
Aiton Goldman <agoldma@us.ibm.com>
2016-10-18 13:39:30 -0400

    added app.js and related files

---
+++ front/www/js/app/app.js
@@ -0,0 +1,56 @@
+app = angular.module(
+	'TDApp',
+    [
+        'ionic',
+ 	'ui.router',            
+        'ngCookies',            
+        'ngSanitize',
+        'app.login',
+        'TD_services'
+        /*REPLACEMECHILD*/
+	]
+);
+
+app.controller(
+    'IndexController',    
+    function($scope, $location, $http, 
+             $state,Modals) {
+        //Modals.loading();
+        //Modals.set_status_modal_message('hi there this is a long message but Im sure you can handle it');
+        $scope.site = $state.params.site;
+        //Modals.error('Hi There',$scope.site,'app.login');
+
+        //$scope.site = $state.params.site;
+        //console.log('in index');
+        
+        //Utils.controller_bootstrap($scope,$state);
+        //$scope.User = User;
+    }
+);
+app.controller(
+    'TestController',    
+    function($scope, $location, $http, 
+             $state, $ionicHistory) {
+        $scope.site = $state.params.site;
+        console.log('in test 1');
+//        console.log($ionicHistory.viewHistory());
+        //Utils.controller_bootstrap($scope,$state);
+        //$scope.User = User;
+    }
+);
+
+app.controller(
+    'TestController2',    
+    function($scope, $location, $http, 
+             $state, $ionicHistory) {
+        $scope.site = $state.params.site;
+        console.log('in tests 2');
+        console.log($ionicHistory.viewHistory());
+        //Utils.controller_bootstrap($scope,$state);
+        //$scope.User = User;
+    }
+);
+
+
+
+
---
+++ front/www/js/app/routes.js
@@ -0,0 +1,27 @@
+angular.module('TDApp').config(['$stateProvider', '$urlRouterProvider',function(    
+    $stateProvider,
+    $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/event_select');
+    
+    $stateProvider.state(
+        'event_select', {
+            url: '/event_select',
+            templateUrl: 'js/app/event_select.html'
+        }
+    ).state(
+        'app', {
+            url: '/:site/app',
+            views: {
+                '@': {
+                    templateUrl: 'js/app/home.html',
+                    controller: 'IndexController'
+                },
+                'menuContent@app': {
+                    templateUrl: 'js/app/front.html'                    
+                }
+
+            }
+        }
+    );
+}]);
+

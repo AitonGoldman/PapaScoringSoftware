bab441c38bbea46f90cbfca14e4720c1a160ae81
Aiton Goldman <agoldma@us.ibm.com>
2016-10-18 14:42:45 -0400

    removed some stuff that should not have gotten in

--- front/www/js/app/login.old/login.js
+++
@@ -1,5 +0,0 @@
-angular.module('app.login',['app.login.process'])
-    .controller('app.login', function($scope, $ionicModal, $timeout) {
-        $scope.user = {};
-        console.log('in logins');
-});

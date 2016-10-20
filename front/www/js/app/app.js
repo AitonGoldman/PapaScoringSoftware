a8088c1cd95c8b945954c2c7f7324b6490de487d
Aiton Goldman <agoldma@us.ibm.com>
2016-10-20 08:46:45 -0400

    added cors stuff

--- front/www/js/app/app.js
+++ front/www/js/app/app.js
@@ -21,6 +21,11 @@ app.controller(
     }
 );
 
+app.config(function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];   
+});
 
 
 

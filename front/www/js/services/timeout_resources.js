0db50043755c25d5fcfc1b0a9a21165a4e155b7c
Aiton Goldman <agoldma@us.ibm.com>
2016-11-11 07:27:20 -0500

    added division machine routes to timeout_resources

--- front/www/js/services/timeout_resources.js
+++ front/www/js/services/timeout_resources.js
@@ -153,12 +153,15 @@ angular.module('TD_services.timeout_resources')
                   var getTournamentResource = generate_resource_definition(':site/tournament/:tournament_id',
                                                                            'GET');
                   var getTournamentDivisionsResource = generate_resource_definition(':site/tournament/:tournament_id/division',
-                                                                     'GET');                                                      
+                                                                                    'GET');
                   var getDivisionResource = generate_resource_definition(':site/division/:division_id',
                                                                      'GET');                                    
-
-                  
-                  
+                  var addDivisionMachineResource = generate_resource_definition(':site/division/:division_id/division_machine',
+                                                                           'POST');                  
+                  var getDivisionMachinesResource = generate_resource_definition(':site/division/:division_id/division_machine',
+                                                                           'GET');                  
+                  var deleteDivisionMachineResource = generate_resource_definition(':site/division/:division_id/division_machine/:division_machine_id',
+                                                                           'DELETE');                  
                   return {
 	              GetAllResources: function(){
 	                  return resource_results;
@@ -178,7 +181,9 @@ angular.module('TD_services.timeout_resources')
                       AddDivision: generate_custom_http_executor(addDivisionResource,'added_division','post'),
                       GetTournaments: generate_custom_http_executor(getTournamentResource,'tournaments','get'),
                       GetDivision: generate_custom_http_executor(getDivisionResource,'division','get'),
-                      GetTournamentDivisions: generate_custom_http_executor(getTournamentDivisionsResource,'tournament_divisions','get'),                                                    
+                      GetTournamentDivisions: generate_custom_http_executor(getTournamentDivisionsResource,'tournament_divisions','get'),                                              AddDivisionMachine: generate_custom_http_executor(addDivisionMachineResource,'added_division_machine','post'),
+                      DeleteDivisionMachine: generate_custom_http_executor(deleteDivisionMachineResource,'deleted_division_machine','get'),
+                      GetDivisionMachines: generate_custom_http_executor(getDivisionMachinesResource,'division_machines','get'),
                       UpdateDivision: generate_custom_http_executor(updateDivisionResource,'updated_division','post')
             
                   };

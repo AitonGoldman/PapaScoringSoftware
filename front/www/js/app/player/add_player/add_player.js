326e0ddb7f3bb907d0d66d88e83c89a21065e303
Aiton Goldman <agoldma@us.ibm.com>
2016-11-20 07:07:04 -0500

    force pic taking on native app during player creation

--- front/www/js/app/player/add_player/add_player.js
+++ front/www/js/app/player/add_player/add_player.js
@@ -31,7 +31,16 @@ angular.module('app.player.add_player').controller(
                     ActionSheets.choose_ifpa_lookup_action($scope.resources.ifpa_rankings.data.search,$scope.player_info.ifpa_result);                
                 });            
             };
-            
+            $scope.form_not_ready_for_submit = function(){
+                if ($scope.is_native == true && $scope.player_info.has_picture != true){
+                    return true;
+                }
+                if (Utils.var_empty($scope.player_info.first_name) == true || Utils.var_empty($scope.player_info.last_name) == true || Utils.var_empty($scope.player_info.linked_division_id) == true || $scope.player_info.ifpa_result.looked_up != true){                    
+                    return true;
+                } else {
+                    return false;
+                }
+            };
         }
     ]
 );

cd60627a4ee0d8f9008150db19ad15e80ea8702e
Aiton Goldman <agoldma@us.ibm.com>
2016-11-21 17:39:13 -0500

    abstracted out tournament creation

--- back/routes/orm_creation.py
+++ back/routes/orm_creation.py
@@ -1,6 +1,43 @@
 from util import db_util
 from routes.utils import check_roles_exist
 
+def create_tournament(app,tournament_data):
+    db = db_util.app_db_handle(app)
+    tables = db_util.app_db_tables(app)
+
+    new_tournament = tables.Tournament(
+        tournament_name=tournament_data['tournament_name']                        
+    )    
+    if 'single_division' in tournament_data and tournament_data['single_division']:        
+        if 'finals_num_qualifiers' not in tournament_data or tournament_data['finals_num_qualifiers'] == "":            
+            raise BadRequest('finals_num_qualifiers not found in post data')            
+        new_tournament.single_division=True
+        new_division = tables.Division(            
+            division_name = new_tournament.tournament_name+"_single",
+            finals_num_qualifiers = tournament_data['finals_num_qualifiers']
+        )
+        
+        if tournament_data['scoring_type'] == "HERB":
+            new_division.number_of_scores_per_entry=1
+        if 'use_stripe' in tournament_data and tournament_data['use_stripe']:
+            new_division.use_stripe = True
+            new_division.stripe_sku=tournament_data['stripe_sku']
+        if 'local_price' in tournament_data and tournament_data['use_stripe'] == False: 
+            new_division.local_price=tournament_data['local_price']
+        if 'team_tournament' in tournament_data and tournament_data['team_tournament']:    
+            new_division.team_tournament = True
+        else:
+            new_division.team_tournament = False    
+        new_division.scoring_type=tournament_data['scoring_type']
+            
+        db.session.add(new_division)
+        new_tournament.divisions.append(new_division)        
+    else:
+        new_tournament.single_division=False    
+    db.session.add(new_tournament)
+    db.session.commit()
+    return new_tournament
+    
 def create_roles(app,custom_roles=[]):
     roles = ['admin','desk','scorekeeper','void','player']                    
     db_handle = app.tables.db_handle

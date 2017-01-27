00cdc08e133082c5c9c2a99c013b726c1231d101
Aiton Goldman <agoldma@us.ibm.com>
2017-01-26 23:26:42 -0500

    fixed back button on ios

--- front/www/js/app/app.js
+++ front/www/js/app/app.js
@@ -116,12 +116,6 @@ app.controller(
         $scope.is_native = false;
         $scope.uploaded_pic_name=undefined;
         $ionicPlatform.ready(function() {        
-            dev_info = ionic.Platform.device();
-            
-            if (_.size(dev_info)!=0){
-                $scope.is_native=true;
-                //alert('on a native app');
-            }
         });
         $scope.force_reload = function(){
             divisions_promise = TimeoutResources.GetDivisions(undefined,{site:$scope.site});            
@@ -219,19 +213,25 @@ app.controller(
 );
 
 app.run(function($ionicPlatform,$rootScope) {
-  $ionicPlatform.ready(function() {
+    $ionicPlatform.ready(function() {
+        ionic.Platform.showStatusBar(false);        
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-
+      dev_info = ionic.Platform.device();
+      console.log(dev_info);
+      if (_.size(dev_info)!=0){
+          $scope.is_native=true;          
+      }
+      if(window.cordova && window.cordova.plugins.Keyboard) {
   //Change this to false to return accessory bar 
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-    }
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+      }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
+
     $rootScope.$on('cloud:push:notification', function(event, data) {
         var msg = data.message;
         alert(msg.title + ': ' + msg.text);
@@ -255,9 +255,10 @@ app.config(function($httpProvider,$ionicConfigProvider,$ionicCloudProvider) {
     $httpProvider.defaults.withCredentials = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
     $ionicConfigProvider.backButton.previousTitleText(false);
-    $ionicConfigProvider.backButton.text(" ");
+    $ionicConfigProvider.backButton.text(" ");        
     $ionicConfigProvider.backButton.icon('ion-arrow-left-a');
- $ionicCloudProvider.init({
+
+    $ionicCloudProvider.init({
     "core": {
       "app_id": "a302e6bc"
     },

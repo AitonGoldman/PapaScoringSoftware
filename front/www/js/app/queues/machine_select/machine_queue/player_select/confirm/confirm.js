b9f04f1248bd98cb1f8d1d981b86ff21ca9109d3
Aiton Goldman <agoldma@us.ibm.com>
2017-01-26 20:32:27 -0500

    ui prevents player without tokens from queing up

--- front/www/js/app/queues/machine_select/machine_queue/player_select/confirm/confirm.js
+++ front/www/js/app/queues/machine_select/machine_queue/player_select/confirm/confirm.js
@@ -10,13 +10,23 @@ angular.module('app.queues.machine_select.machine_queue.player_select.confirm').
 	    $scope.division_machine_name=$state.params.division_machine_name;        
             $scope.player_id=$state.params.player_id;
             $scope.player_name = $state.params.player_name;
-            $scope.utils = Utils;
+            $scope.utils = Utils;           
             Modals.loading();
             $scope.bootstrap_promise = $scope.controller_bootstrap($scope,$state);                            
-            $scope.bootstrap_promise.then(function(data){
+            token_promise = TimeoutResources.GetPlayerTokens($scope.bootstrap_promise,{site:$scope.site,player_id:$scope.player_id});
+            token_promise.then(function(data){
                 if(User.logged_in()==true){
                     $scope.logged_in_player_id = User.logged_in_user().player_id;
                 }
+                $scope.resources = TimeoutResources.GetAllResources();
+                if($scope.resources.divisions.data[$scope.division_id].meta_division_id != undefined){
+                    meta_division_id = $scope.resources.divisions.data[$scope.division_id].meta_division_id;
+                    $scope.num_tokens_left = $scope.resources.player_tokens.data.tokens.metadivisions[meta_division_id];                    
+                } else {
+                    $scope.num_tokens_left = $scope.resources.player_tokens.data.tokens.divisions[$scope.division_id];                    
+                    
+                }
+                console.log($scope.num_tokens_left);
                 Modals.loaded();
             });
     }]

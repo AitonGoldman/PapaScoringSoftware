88a447b337b4505755266d32c9924144d8a891b4
Aiton Goldman <agoldma@us.ibm.com>
2017-01-29 10:25:11 -0500

    fixed populate scripts to be more flexible

--- back/utils/populate_division_discount_fields_via_curl.sh
+++ back/utils/populate_division_discount_fields_via_curl.sh
@@ -1,5 +1,6 @@
-curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"1\",\"discount_ticket_count\":\"3\"}" http://localhost:8000/elizabeth/division/1
-curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"1\",\"discount_ticket_count\":\"3\"}" http://localhost:8000/elizabeth/division/2
-curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"1\",\"discount_ticket_count\":\"3\"}" http://localhost:8000/elizabeth/division/3
-curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"1\",\"discount_ticket_count\":\"3\"}" http://localhost:8000/elizabeth/division/5
-curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"discount_ticket_count\":\"3\"}" http://localhost:8000/elizabeth/meta_division/1
+curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"1\",\"discount_ticket_count\":\"3\"}" $POPULATE_URL:8000/elizabeth/division/1
+curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"2\",\"discount_ticket_count\":\"3\"}" $POPULATE_URL:8000/elizabeth/division/2
+curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"3\",\"discount_ticket_count\":\"3\"}" $POPULATE_URL:8000/elizabeth/division/3
+curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"3\",\"discount_ticket_count\":\"3\"}" $POPULATE_URL:8000/elizabeth/division/4
+curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"4\",\"discount_ticket_count\":\"3\"}" $POPULATE_URL:8000/elizabeth/division/5
+curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"discount_ticket_count\":\"3\"}" $POPULATE_URL:8000/elizabeth/meta_division/1
--- back/utils/populate_division_num_herb_entries.sh
+++ back/utils/populate_division_num_herb_entries.sh
@@ -1,1 +1,1 @@
-seq 1 8 | xargs -n1 -Izzz curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"zzz\",\"number_of_relevant_scores\":\"6\"}" http://localhost:8000/elizabeth/division/zzz
+seq 1 8 | xargs -n1 -Izzz curl -b /tmp/cookie -H "Content-Type: application/json" -X PUT -d "{\"division_id\":\"zzz\",\"number_of_relevant_scores\":\"6\"}" $POPULATE_URL:8000/elizabeth/division/zzz

82e30c7531e1309680455033d38cc855763bb7d1
Aiton Goldman <agoldma@us.ibm.com>
2017-02-11 21:19:23 -0500

    added bare init test

---
+++ back/tests/integration/test_setup_bare_init.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+from mock import MagicMock
+import td_integration_test_base
+import json
+from routes import orm_creation
+from util import db_util
+import random
+
+class SetupBareInitTD(td_integration_test_base.TdIntegrationSetupTestBase):
+    def setUp(self):
+        super(SetupAddToMachineTD,self).setUp()
+        response,results = self.dispatch_request('/%s/util/healthcheck' % self.poop_db_name)                
+        self.flask_app = self.app.instances[self.poop_db_name]
+        self.admin_user, self.scorekeeper_user,self.desk_user = orm_creation.create_stanard_roles_and_users(self.flask_app)        
+        db_util.load_machines_from_json(self.flask_app,True)
+                
+    def test_setup_before_token(self):        
+        pass

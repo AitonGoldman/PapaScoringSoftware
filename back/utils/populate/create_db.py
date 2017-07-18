f11f63164290ae8c3da61515e291365b2f90f425
Aiton Goldman <agoldma@us.ibm.com>
2017-07-18 18:55:39 -0400

    create util creates users and roles

--- back/utils/populate/create_db.py
+++ back/utils/populate/create_db.py
@@ -1,6 +1,7 @@
 from util import db_util
 from flask import Flask
 from util.db_info import DbInfo
+from routes.orm_creation import create_stanard_roles_and_users
 import os,sys
 dummy_app = Flask('dummy_app')                
 if len(sys.argv) > 1:
@@ -9,3 +10,5 @@ else:
     print "didn't specify db name..."
     sys.exit(1)
 db_util.create_db_and_tables(dummy_app,poop_db_name,DbInfo({'DB_TYPE':'postgres','DB_USERNAME':os.getenv('DB_USERNAME'),'DB_PASSWORD':os.getenv('DB_PASSWORD')}),drop_tables=False)
+create_stanard_roles_and_users(dummy_app)
+db_util.load_machines_from_json(dummy_app)

760435af061c8039dac2d1df27d97fff7c7c58a6
Aiton Goldman <agoldma@us.ibm.com>
2017-08-20 20:39:25 -0400

    made run_tests.sh verbose

--- back/run_tests.sh
+++ back/run_tests.sh
@@ -5,4 +5,4 @@ export db_password='fakepassword'
 #export db_name='test_db'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PYTHONPATH=$DIR python -m unittest discover -s $DIR/test/unit -f
-PYTHONPATH=$DIR python -m unittest discover -s $DIR/test/integration -f
+PYTHONPATH=$DIR python -m unittest discover -v $DIR/test/integration -f

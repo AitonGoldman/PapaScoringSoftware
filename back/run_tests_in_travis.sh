02e348d9ba79a0328f3de7db5a474484438be29c
Aiton Goldman <agoldma@us.ibm.com>
2017-08-20 21:05:18 -0400

    changed name of run_tests.sh

--- back/run_tests_in_travis.sh
+++ back/run_tests_in_travis.sh
@@ -1,8 +1,6 @@
-#rm /tmp/test_db*
 export db_type='postgres'
 export db_username='postgres'
 export db_password='fakepassword'
-#export db_name='test_db'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PYTHONPATH=$DIR python -m unittest discover -s $DIR/test/unit -f
 PYTHONPATH=$DIR python -m unittest discover -s $DIR/test/integration -f

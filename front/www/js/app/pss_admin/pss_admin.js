d909976dc07c24bc6e260cd200872421a71c1e54
Aiton Goldman <agoldma@us.ibm.com>
2017-09-07 18:35:38 -0400

    login success screen added

--- front/www/js/app/pss_admin/pss_admin.js
+++ front/www/js/app/pss_admin/pss_admin.js
@@ -14,7 +14,12 @@ angular.module('pss_admin').controller(
             $scope.bootstrap();
             $scope.login_func = function(){
                 $scope.post_success = false;
-                var on_success = function(data){$scope.logged_in_user=data;credentialsService.set_pss_user_credentials("pss_admin",data);};                
+                var on_success = function(data){
+                    $scope.logged_in_user=data['pss_user'];
+                    credentialsService.set_pss_user_credentials("pss_admin",data);
+                    $scope.post_success = true;
+                };
+                
                 var on_failure = resourceWrapperService.stay_on_current_state_for_error;            
                 var prom =resourceWrapperService.get_wrapper_with_loading('post_pss_admin_login',on_success,on_failure,{},{username:$scope.pss_user.username,password:$scope.pss_user.password});            
 

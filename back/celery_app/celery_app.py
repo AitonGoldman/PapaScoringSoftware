c9642eb3b7250c8ff7506b04879981c578705794
Aiton Goldman <agoldma@us.ibm.com>
2018-07-11 13:16:42 -0400

    fixed push notifications not having sound

--- back/celery_app/celery_app.py
+++ back/celery_app/celery_app.py
@@ -11,9 +11,7 @@ def send_indiv_message(title,msg,token):
     push_service = FCMNotification(api_key=fcm_api_key)    
     message_title = title
     message_body = msg
-    #time.sleep(10)
-    result = push_service.notify_single_device(registration_id=token, message_title=message_title, message_body=message_body)    
-    #result = push_service.notify_topic_subscribers(topic_name="all", message_body=message_body, message_title=message_title)    
+    result = push_service.notify_single_device(registration_id=token, message_title=message_title, message_body=message_body,sound='default')        
     return None
 
     
